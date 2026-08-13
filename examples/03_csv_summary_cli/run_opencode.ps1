15ca74ceca9bebbb8877ee23aa9b078a4992b603
Kevin <kevin840307@gmail.com>
2026-08-13 19:55:31 +0800

    ㄎ

--- examples/03_csv_summary_cli/run_opencode.ps1
+++
@@ -1,8 +0,0 @@
-$goal = Get-Content -Raw "examples/03_csv_summary_cli/prompt.txt"
-python ai_task_runner.py `
-  --backend opencode `
-  --command opencode.exe `
-  --project-root "examples/03_csv_summary_cli/project" `
-  --goal $goal `
-  --validator "examples/03_csv_summary_cli/validator.py" `
-  --force-new
